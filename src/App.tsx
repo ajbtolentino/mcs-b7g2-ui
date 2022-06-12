9fb7f407a22c224d8f69465e703d9cc172bb6e8b
Allan Tolentino <allanto@magenic.com>
2022-06-12 20:49:12 +0800

    Added menu endpoint

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,12 @@
-import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import { Dashboard } from './components/Customer/Dashboard';
-import { Enter } from './components/Customer/Enter';
-import { Layout } from './components/Layout';
-import { NotFound } from './components/NotFound';
+import { MenuContextProvider } from './context/menu/menuContextProvider';
 
 function App() { 
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path="enter" element={<Enter />}/>
-          <Route path="dashboard" element={<Dashboard />}/>
-          <Route path="*" element={<NotFound />}/>
-        </Route>
-      </Routes>
-    </>
+    <MenuContextProvider>
+      <Dashboard />
+    </MenuContextProvider>
   );
 }
 
